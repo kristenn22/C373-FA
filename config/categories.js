d59c46c8ea85cf8e91556bd9780d762218a265b6
cherkoh <23011910@myrp.edu.sg>
2026-01-23 20:41:50 +0800

    Add files via upload

---
+++ config/categories.js
@@ -0,0 +1,25 @@
+// config/categories.js
+const categories = [
+    'Electronics',
+    'Fashion',
+    'Beauty',
+    'Home & Living',
+    'Books & Stationery',
+    'Sports & Outdoors',
+    'Toys & Collectibles',
+    'Pets',
+    'Services',
+    'Vehicles',
+    'Others'
+];
+
+const topCategories = [
+    'Electronics',
+    'Fashion',
+    'Beauty',
+    'Home & Living',
+    'Books & Stationery',
+    'Sports & Outdoors'
+];
+
+module.exports = { categories, topCategories };
